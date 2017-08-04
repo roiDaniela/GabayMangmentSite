aa57036643a7d618d77ae98d440eb00d226a848e
erankatz <erankatz91@gmail.com>
2017-08-04 16:59:54 +0300

    Fixed Drag and drop

--- WingtipToys/AliyotHistory.aspx.cs
+++ WingtipToys/AliyotHistory.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using GabayManageSite.GabayDataSetTableAdapters;
+using System.Xml;
 
 namespace GabayManageSite
 {
@@ -14,9 +15,26 @@ namespace GabayManageSite
         PrayersTableAdapter PrayersTableAdapter { get; set; }
         ParashaDetailsTableAdapter ParashaTabeleAdapter { get; set;}
         AliyaHistoryTableAdapter AliyaHistoryAdapter { get; set; }
+        GabayDataSet gabayDataSet { get; set; }
+        ParashotTableAdapter parashaTableAdapter { get; set; }
+        GabayDataSet.ParashotDataTable currentParasha { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             dlProducts.ItemStyle.BorderStyle = BorderStyle.Dashed;
+            parashaTableAdapter = new GabayDataSetTableAdapters.ParashotTableAdapter();
+            currentParasha = parashaTableAdapter.GetLastParashah();
+
+            HyperLinkParasha.Text = currentParasha.Rows[0]["NameHe"].ToString();
+            HyperLinkParasha.NavigateUrl = currentParasha.Rows[0]["link"].ToString().TrimEnd(' ');
+            DateTime parashaDate = (DateTime)currentParasha.Rows[0]["date"];
+
+            String URLString = "http://www.hebcal.com/converter/?cfg=xml&gy=" + parashaDate.Year + "&gm=" + parashaDate.Month + "&gd=" + parashaDate.Day + "&g2h=1";
+
+            XmlTextReader reader = new XmlTextReader(URLString);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(reader);
+            LabelHebDate.Text = doc.ChildNodes[1].ChildNodes[1].Attributes[3].Value;
         }
 
 
@@ -26,6 +44,11 @@ namespace GabayManageSite
             AliyaHistoryAdapter = new AliyaHistoryTableAdapter();
             AliyaHistoryAdapter.DeleteLastAliyaDateRecords((int)Session["currSynId"]);
             Response.Redirect(Request.RawUrl);
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
